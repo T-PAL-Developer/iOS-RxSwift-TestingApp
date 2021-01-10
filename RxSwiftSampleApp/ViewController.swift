7c108af7bbe2f0cb83888e6c689b351a99bef7d2
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-01-10 22:18:02 +0100

    Add func submitButton

--- RxSwiftSampleApp/ViewController.swift
+++ RxSwiftSampleApp/ViewController.swift
@@ -10,18 +10,55 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var helloLabel: UILabel!
     @IBOutlet weak var nameEntryTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var namesLabel: UILabel!
     
+    let disposeBag = DisposeBag()
+    var namesArray: BehaviorRelay<[String]> = BehaviorRelay(value: [])
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configUI()
+        bindTextFieldWithLabel()
+        bindSubmitButton()
+        
     }
-
-
+    
+    func bindTextFieldWithLabel() {
+        nameEntryTextField.rx.text
+            //.debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+            .map {
+                if $0 == "" {
+                    return "Type your name bellow."
+                }
+                else {
+                    return "Hello, \($0!)."
+                }
+            }
+            .bind(to: helloLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    func bindSubmitButton() {
+        submitButton.rx.tap.subscribe(onNext: {
+            if self.nameEntryTextField.text != "" {
+                self.namesArray.accept(self.namesArray.value + [self.nameEntryTextField.text!])
+                self.namesLabel.rx.text.onNext(self.namesArray.value.joined(separator: ", "))
+                self.nameEntryTextField.rx.text.onNext("")
+                self.nameEntryTextField.rx.text.onNext("Type your name bellow.")
+            }
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    func configUI() {
+        nameEntryTextField.layer.cornerRadius = 10
+        submitButton.layer.cornerRadius = 10
+    }
+    
 }
 
